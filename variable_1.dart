c5fdd77d62fe807fbd28b62e6d2f13b3b93de6b8
SANJI0722 <saepullohepul565@gmail.com>
2022-11-24 17:49:35 +0700

    Belajar Dart Dasar

---
+++ variable_1.dart
@@ -0,0 +1,21 @@
+void main() {
+  /*
+  variable merupakan tempat untuk menyimpan data, berguna untuk kita menggunakan data berkali-kali.
+  variable wajib memiliki tipe data dan nama variable.
+  */
+  print('Nurul');
+
+  String nama;
+  nama = 'Fitri';
+
+  print(nama);
+
+  /*
+  ini adalah variable dengan deklarasi langsung
+  */
+  String firstName = 'Tuti';
+  String lastName = 'Awaliyah';
+
+  print(firstName);
+  print(lastName);
+}
